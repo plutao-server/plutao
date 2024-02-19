9ab1fcfd60e8462ab1e4f9f328def23be75d3224
Shishu <shishudesu@gmail.com>
2024-02-19 11:38:21 +0530

    Added Dragging callbacks for world

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,60 @@
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class Player extends PositionComponent{
-  static final _paint = Paint()..color = Colors.white;
+class WorldSprite extends SpriteComponent{
   
-  @override
-  void render(Canvas canvas){
-    canvas.drawRect(size.toRect(), _paint);
-  }
+  
+  
+  
 }
 
-class PlutaoGame extends FlameGame {
+class PlutaoWorld extends World with DragCallbacks{
+  bool _isDragged = false;
+  Vector2 camPos = Vector2(0.0, 0.0);
+  
+  @override
+  Future<void> onLoad() async{
+    final bgSprite = SpriteComponent(size: Vector2.all(1024.0) , sprite: await Sprite.load("stardew-valley-test.jpg"), anchor: Anchor.center);
+     CameraComponent.currentCamera?.moveTo(camPos);
+    await add(bgSprite);
+  }
+  @override
+  void render(Canvas canvas) {
+    // TODO: implement render
+    CameraComponent.currentCamera?.moveTo(camPos);
+  }
+  @override
+  void onDragStart(DragStartEvent event){
+    _isDragged = true;
+  }
+  @override
+  void onDragEnd(DragEndEvent event) => _isDragged = false;
+  @override void onDragUpdate(DragUpdateEvent event) {
+    // TODO: implement onDragUpdate
+    camPos -= event.localDelta;
+  }
+  
+}
+
+class PlutaoGame extends FlameGame<PlutaoWorld> with SingleGameInstance {
+  late final SpriteComponent test_obj;
+  
+  PlutaoGame(PlutaoWorld world): super(world: world);
+  
   @override
   Future<void> onLoad() async{
     await super.onLoad();
-    add(
-      Player()
-      ..position = size/2
-      ..width = 50
-      ..height = 100
-      ..anchor = Anchor.center
-    );
+    await images.load('stardew-valley-test.jpg');
+    camera.viewfinder.anchor = Anchor.center;
+     
+    
   }
 }
 
 void main(){
-  final game = FlameGame();
-  runApp(GameWidget(game: PlutaoGame()));
+  final mGame = PlutaoGame( PlutaoWorld());
+  runApp(GameWidget(game: mGame));
 }
