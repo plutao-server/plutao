2bb4fcc9c24bf7c3dd303e2dc4d2e44b7b83b77d
Shishu <shishudesu@gmail.com>
2024-02-20 15:50:13 +0530

    Added Tiling
    - Added flame_tiled
    - Added assets created using Tiled
    - Added currently hovered tile

---
+++ assets/tiles/map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="map" tilewidth="32" tileheight="32" tilecount="880" columns="40">
+ <image source="./map.png" width="1280" height="720"/>
+</tileset>
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:flame/camera.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -9,15 +11,25 @@ class WorldSprite extends SpriteComponent {}
 class PlutaoWorld extends World with DragCallbacks {
   bool _isDragged = false;
   Vector2 camPos = Vector2(0.0, 0.0);
+  TiledComponent? tiledMap;
+  late Vector2 canvasSize;
+  Vector2? prevMousePositionTiledMap;
+  Gid placeableId = Gid.fromInt(881);
+  Gid emptyId = Gid.fromInt(0);
+  Gid? unplaceableId;
 
   @override
   Future<void> onLoad() async {
-    final bgSprite = SpriteComponent(
-        size: Vector2(1280.0, 720.0),
-        sprite: await Sprite.load("map.png"),
-        anchor: Anchor.center);
+    // final bgSprite = SpriteComponent(
+    //     size: Vector2(1280.0, 720.0),
+    //     sprite: await Sprite.load("map.png"),
+    //     anchor: Anchor.center);
+    tiledMap = await TiledComponent.load("map.tmx", Vector2.all(32.0));
+    tiledMap!.anchor = Anchor.center;
+    var objectLayer = tiledMap!.tileMap.getLayer<TileLayer>("Object Layer");
+
     CameraComponent.currentCamera?.moveTo(camPos);
-    await add(bgSprite);
+    await add(tiledMap!);
   }
 
   @override
@@ -38,9 +50,52 @@ class PlutaoWorld extends World with DragCallbacks {
     // TODO: implement onDragUpdate
     camPos -= event.localDelta;
   }
+
+  void onMouseMove(PointerHoverInfo info) {
+    if (tiledMap != null) {
+      // var data = tiledMap.tileMap.getTileData(layerId: 1, x: info.eventPosition.global.x.toInt(), y: info.eventPosition.global.y.toInt());
+      // var data =tiledMap.tileMap.getTileData(layerId: 1, x: 10, y: 0);
+      // var objectLayer = tiledMap.tileMap.getLayer<TileLayer>("Object Layer");
+      // print(info.eventPosition.widget.x.toInt());
+      // print(info.eventPosition.widget.y.toInt());
+      //  print(data?.tile);
+
+      //print(tiledMap.topLeftPosition + camPos);
+      
+      final tlp =tiledMap!.topLeftPosition -camPos;
+      final mp = Vector2(info.eventPosition.widget.x - canvasSize.x / 2,
+          (info.eventPosition.widget.y - canvasSize.y / 2));
+      final brp = tlp + tiledMap!.scaledSize; //bottom right of the map
+
+      if (mp.x < tlp.x || mp.y < tlp.y || mp.x > brp.x || mp.y > brp.y) {
+        print("Out of map");
+      } else {
+        final tsp = (mp + camPos + tiledMap!.scaledSize/2)/32;
+        print(tsp);
+        
+        if (prevMousePositionTiledMap != null) {
+          tiledMap!.tileMap.setTileData(
+              layerId: 1,
+              x: prevMousePositionTiledMap!.x.toInt(),
+              y: prevMousePositionTiledMap!.y.toInt(),
+              gid: emptyId);
+        }
+        tiledMap!.tileMap.setTileData(
+            layerId: 1, x: tsp.x.toInt(), y: tsp.y.toInt(), gid: placeableId);
+        prevMousePositionTiledMap = tsp;
+      }
+    }
+  }
+
+  Vector2 tilePostoMouse(Vector2 tp) {
+    //based on number of tiles and tile size
+    tp = tp - canvasSize / 2;
+    return tp;
+  }
 }
 
-class PlutaoGame extends FlameGame<PlutaoWorld> with SingleGameInstance {
+class PlutaoGame extends FlameGame<PlutaoWorld>
+    with SingleGameInstance, MouseMovementDetector {
   late final SpriteComponent test_obj;
 
   PlutaoGame(PlutaoWorld world) : super(world: world);
@@ -50,6 +105,17 @@ class PlutaoGame extends FlameGame<PlutaoWorld> with SingleGameInstance {
     await super.onLoad();
     await images.load('stardew-valley-test.jpg');
     camera.viewfinder.anchor = Anchor.center;
+    world.canvasSize = canvasSize;
+  }
+
+  @override
+  void onMouseMove(PointerHoverInfo info) {
+    world.onMouseMove(info);
+  }
+
+  @override
+  void onGameResize(Vector2 size) {
+    super.onGameResize(size);
   }
 }
 
